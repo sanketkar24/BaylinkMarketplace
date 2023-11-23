f5c4a5b29613ddf0ce5884ec46e7a1e7f997d85c
Sakshat Gandhi <sakshatgandhi@Sakshats-MacBook-Air.local>
2023-11-23 22:39:02 +0530

    Added logo's and padding x

--- app/components/navigation/ourBrands/index.js
+++ app/components/navigation/ourBrands/index.js
@@ -4,7 +4,9 @@ import Marquee from "react-fast-marquee";
 import PARADYES from '../../../../images/paradyes_logo.png';
 import ALPINO from '../../../../images/alpino.png';
 import OFFO_STORE from '../../../../images/offo_store.jpg';
-import SLEEP_OWL from '../../../../images/sleepy_owl.png';
+import SLEEPY_OWL from '../../../../images/sleepy_owl.png';
+import PEPPER_ORIGINALS from '../../../../images/pepper_originals.webp';
+import FOMO from '../../../../images/fomo_logo.jpeg';
 
 import Image from 'next/image';
 function brands() {
@@ -12,15 +14,15 @@ function brands() {
     <>
       <main className='flex flex-col justify-center items-center h-[100%] bg-orange-100'>
         <h1 className="text-4xl pt-20 pb-5">Our <span className="font-semibold bg-blue-300 px-1 rounded-md ">Brands</span></h1>
-        <p className='font-light tablet:text-base text-sm '>Discover the brands thriving through our services in the retail marketplace.</p>
+        <p className='px-5 font-light tablet:text-base text-sm '>Discover the brands thriving through our services in the retail marketplace.</p>
         <div className="my-10 w-full">
           <Marquee speed={"60"} pauseOnHover={true} className='align-middle'>
             <Image src={PARADYES} className='mx-10 w-2/3' alt="" preview={false} height={80} ></Image>
             <Image src={ALPINO} className='mx-10 w-2/3' alt="" preview={false} height={80} ></Image>
             <Image src={OFFO_STORE} className='mx-10 w-2/3' alt="" preview={false} height={80} ></Image>
-            <Image src={SLEEP_OWL} className='mx-10 w-2/3' alt="" preview={false} height={80} ></Image>
-            <Image src={ALPINO} className='mx-10 w-2/3' alt="" preview={false} height={80} ></Image>
-            <Image src={OFFO_STORE} className='mx-10 w-2/3' alt="" preview={false} height={80} ></Image>
+            <Image src={SLEEPY_OWL} className='mx-10 w-2/3' alt="" preview={false} height={80} ></Image>
+            <Image src={FOMO} className='mx-10 w-2/3' alt="" preview={false} height={80} ></Image>
+            <Image src={PEPPER_ORIGINALS} className='mx-10 w-2/3' alt="" preview={false} height={80} ></Image>
           </Marquee>
         </div>
       </main>
